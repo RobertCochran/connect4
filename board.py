10d43f62a3113115a522cd971575dddf7a74bd95
13YannBoat <pavlenko.yann@gmail.com>
2016-01-21 09:33:42 -0800

    Update board.py

--- board.py
+++ board.py
@@ -1,22 +1,22 @@
-NONE = '.'
-RED = 'R'
-BLACK = 'B'
+empty = '_'
+red = 'R'
+black = 'B'
 
 BOARD_COLUMNS = 7
 BOARD_ROWS = 6
 
 
-def _new_game_board():
+def start_new_board_game()
     '''
     Creates a new game board.  Initially, a game board has the size
     BOARD_COLUMNS x BOARD_ROWS and is comprised only of strings with the
-    value NONE
+    value empty
     '''
     board = []
 
     for col in range(BOARD_COLUMNS):
         board.append([])
         for row in range(BOARD_ROWS):
-            board[-1].append(NONE)
+            board[-1].append(empty)
 
     return board
